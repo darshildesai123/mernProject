00f72f18851cb84ad363e187505711afe516b524
darshildesai123 <desaidarshil567@gmail.com>
2023-12-30 11:49:55 +0530

    my first mern project

---
+++ src/app.js
@@ -0,0 +1,123 @@
+require("dotenv").config();
+
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
+const app = express();
+const bcrypt = require("bcryptjs");
+
+require("./db/conn");
+const Register = require("./registers");
+
+const port = process.env.PORT || 3000;
+
+const static_path = path.join(__dirname, "../public");
+const template_path = path.join(__dirname, "../templates/views");
+const partials_path = path.join(__dirname, "../templates/partials");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(static_path));
+
+app.set("view engine", "hbs");
+
+app.set("views", template_path);
+
+hbs.registerPartials(partials_path);
+
+// console.log(process.env.SECRET_KEY);
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// CRUD Operations
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+// create a new user in our database
+app.post("/register", async (req, res) => {
+  try {
+    // console.log(req.body.firstname);
+    // res.send(req.body.firstname);
+    const password = req.body.password;
+    const confirmpassword = req.body.confirmpassword;
+
+    if (password === confirmpassword) {
+      const registerEmployee = new Register({
+        firstName: req.body.firstname,
+        lastName: req.body.lastname,
+        email: req.body.email,
+        gender: req.body.gender,
+        phone: req.body.phone,
+        age: req.body.age,
+        password: req.body.password,
+        confirmpassword: req.body.confirmpassword,
+      });
+
+      console.log("the success part" + registerEmployee);
+
+      const token = await registerEmployee.generateAuthToken();
+      console.log("the token part" + token);
+
+      const registered = await registerEmployee.save();
+      res.status(201).render("index");
+      // res.status(201).json({status:"succuss",data:registerEmployee});
+    } else {
+      res.send("Passwords are not matching");
+    }
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// login check
+
+app.post("/login", async (req, res) => {
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const useremail = await Register.findOne({ email: email });
+
+    const isMatch = await bcrypt.compare(password, useremail.password);
+
+    const token = await useremail.generateAuthToken();
+    console.log("the token part" + token);
+
+    if (isMatch) {
+      res.status(201).render("index");
+    } else {
+      res.send("invalid login details");
+    }
+  } catch (error) {
+    res.status(400).send("invalid login details");
+  }
+});
+
+// const jwt = require("jsonwebtoken");
+
+// const createToken = async () => {
+//   const token = await jwt.sign(
+//     { _id: "658e897c64bd47f2852fad29" },
+//     "aczqwertyuioplkjhgfdsazxcvbnmkoi",
+//     {
+//       expiresIn: "2 seconds",
+//     }
+//   );
+//   console.log(token);
+
+//   const userVer = await jwt.verify(token, "aczqwertyuioplkjhgfdsazxcvbnmkoi");
+//   console.log(userVer);
+// };
+
+// createToken();
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
